e26ecddd8e3198990e3488e3d22bd5b31bc64a85
nehalkamble <kamblenehal12345678@gmail.com>
2022-03-07 01:07:21 +0530

    update

---
+++ project/3_IMPLEMENTATION/SRC/doorLock.c
@@ -0,0 +1,96 @@
+#include "doorLock.h"
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+void LCD_String(char *str);
+void LCD_Init(void);
+extern int ro1(void);
+extern int ro2(void);
+extern int ro3(void);
+int main(){
+A1:    count=0;
+    int password[]={1,2,3,4};
+    LCD_Clear(); 
+	LCD_Init();	 
+	int flag = 0;
+    LCD_String("H");
+    _delay_ms(200);
+    LCD_String("e");
+     _delay_ms(200);
+     LCD_String("l");
+    _delay_ms(200);
+    LCD_String("l");
+     _delay_ms(200);
+     LCD_String("o");
+     _delay_ms(200);
+     LCD_String(".");
+    _delay_ms(200);
+    LCD_String(".");
+     _delay_ms(200);
+     LCD_String(".");
+     _delay_ms(1000);
+A2:    LCD_Clear();
+	LCD_String("E");
+    _delay_ms(200);
+    LCD_String("n");
+     _delay_ms(200);
+     LCD_String("t");
+    _delay_ms(200);
+    LCD_String("e");
+     _delay_ms(200);
+     LCD_String("r");
+    _delay_ms(200);
+    LCD_String(" ");
+     _delay_ms(200);
+     LCD_String("P");
+    _delay_ms(200);
+    LCD_String("a");
+     _delay_ms(200);
+     LCD_String("s");
+    _delay_ms(200);
+    LCD_String("s");
+     _delay_ms(200);
+     LCD_String("w");
+    _delay_ms(200);
+    LCD_String("o");
+     _delay_ms(200);
+     LCD_String("r");
+    _delay_ms(200);
+    LCD_String("d");
+     _delay_ms(2000);
+     LCD_Clear();
+    DDRC = 0x00;
+    DDRD = 0xff;
+   
+    while(1)
+    {
+        if(count==4){
+            if(memcmp(pass,password,sizeof(pass))==0){
+                LCD_Clear();
+                LCD_String("WELCOME");
+                count=0;
+                _delay_ms(3000);
+                LCD_Clear();
+                goto A1;
+            }
+            else{
+                LCD_Clear();
+            LCD_String("wrong password");
+            count=0;
+            _delay_ms(3000);
+            goto A2;
+        }}
+        ro1();
+        _delay_ms(10);
+       ro2();;
+        _delay_ms(10);
+         ro3();
+        _delay_ms(10);
+    
+    }
+    
+ }
---
+++ project/3_IMPLEMENTATION/inc/doorLock.h
@@ -0,0 +1,21 @@
+#ifndef DOOR_LOCK_H
+#define DOOR_LOCK_H
+
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+#endif
+
+
+
+#define keypad_ddr DDRC
+#define keypad_prt PORTC
+#define keypad_pin PINC
+
+#define LCD_ddr DDRD
+#define LCD_prt PORTD
+#define LCD_pin PIND
+
+
+
+
+#endif
